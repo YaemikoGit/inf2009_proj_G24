39c2592c865ae3183f46e9dd172cf4965a856a73
Liew DaiXuan <dx8153@gmail.com>
2026-02-20 23:16:54 +0800

    updating app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, Response
 
 
 #Import other camera modules as needed
-from camera.headcount import generate_frames, detect_faces_and_pose
+from camera.headcount import generate_frames
 
 #Import other sensor modules as needed
 from sensors.temperature import get_temperature 
